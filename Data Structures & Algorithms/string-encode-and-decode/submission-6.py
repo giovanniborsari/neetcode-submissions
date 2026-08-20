5a35a2de2781acfae4bcf19a9d55afcecf7ac4e2
Giovanni Macri Borsari <82288093+giovanniborsari@users.noreply.github.com>
2026-08-20 11:14:28 -0500

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        #Marks start and end of an element
+        encode = "#;"
+
+        for s in strs:
+            #append the next encoded element to string
+            encode = encode + s + "#;"
+
+        return encode
+
+    def decode(self, s: str) -> List[str]:
+
+        decoded = []
+        for c in s:
+            #Check for empty string
+            if (s == "#;") :
+                break
+            #Find first ocurrence of #;
+            firstIndex = s.find("#;")
+            #Get a substring after first index
+            s = s[(firstIndex)+2:]
+            secondIndex = s.find("#;")
+            decoded.append(s[:secondIndex])
+            s = s[secondIndex:]
+            
+        
+        return(decoded)
+                
+
