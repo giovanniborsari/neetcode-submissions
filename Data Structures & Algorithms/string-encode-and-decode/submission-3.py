2afa6fc881028dc2a378dd6a6999eda26478f130
Giovanni Macri Borsari <82288093+giovanniborsari@users.noreply.github.com>
2026-08-19 21:05:31 -0500

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+    
+        encode = "#;"
+
+        for s in strs:
+            encode = encode + s + "#;"
+
+        return encode
+
+    def decode(self, s: str) -> List[str]:
+
+        if (s == "#;") :
+            return []
+
+        firstIndex = ""
+        secondIndex = ""
+        decoded = []
+        for c in s:
+            if (s == "#;") :
+                break
+            firstIndex = s.find("#;")
+            if firstIndex >= len(s):
+                break
+            s = s[(firstIndex)+2:]
+            secondIndex = s.find("#;")
+            if secondIndex >= len(s):
+                break
+            decoded.append(s[:secondIndex])
+            s = s[secondIndex:]
+            
+        
+        return(decoded)
+                
+
